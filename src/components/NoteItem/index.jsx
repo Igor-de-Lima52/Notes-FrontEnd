263aa639d324df43e2fddbc7d480b6ac27bcc1d1
Igor <delimaigor781@gmail.com>
2024-07-27 12:24:08 -0300

    First Commit

---
+++ src/components/NoteItem/index.jsx
@@ -0,0 +1,11 @@
+import { FiPlus, FiX} from "react-icons/fi";
+import { Container } from "./styles";
+
+export function NoteItem({ isNew, value, onClick, ...rest}){
+  return(
+    <Container $isnew={isNew}>
+      <input type="text" value={value} readOnly={!isNew} {...rest}/>
+      <button className={isNew ? "button-add" : "button-delete" } type="button" onClick={onClick}>{isNew ? <FiPlus /> : <FiX />}</button>
+    </Container>
+  )
+}
